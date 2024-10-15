8323b065ebaa0e11708e73bf9f97629a075b55dc
Bruno-Regis <149000097+Bruno-Regis@users.noreply.github.com>
2024-10-15 10:57:38 -0300

    Add project files.

---
+++ LeaveManagementSystem.Web/Controllers/HomeController.cs
@@ -0,0 +1,41 @@
+using LeaveManagementSystem.Web.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace LeaveManagementSystem.Web.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            // Define Business logic
+            
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            //
+            // get the data, prepare the data, send the data to the view... =)
+            var model = new ErrorViewModel
+            {
+                RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier
+            };
+            return View(model);
+        }
+    }
+}
+ 
